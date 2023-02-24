16bc2ea65d573f3c471f4baa25dc54caa9e724d5
= <sh843173@dal.ca>
2023-02-23 20:28:24 -0400

    Add weird favicon, Refactor server.js file fetch

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@
 const http = require('http');
 const fs = require('fs');
 const mysql = require('mysql');
+const mime = require('mime-types');
 
 const hostname = '127.0.0.1';
 const port = 2048;
@@ -15,12 +16,45 @@ let dbCreds = fs.readFileSync("server/DB/secret.json", 'utf-8');
 const DB = mysql.createConnection(JSON.parse(dbCreds));
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  let page = fs.readFileSync("app/index.html", 'utf-8');
-  res.end(page);
+  let result = getFile(req.url);
+
+  res.statusCode = result.code;
+  res.setHeader('Content-Type', result.type);
+  res.end(result.data);
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+/**
+ * Retrieves a file within the app
+ * @param {String} path filepath to get file from
+ * @returns An object with the appropriate file data, http code
+ *          and MIME type
+ */
+function getFile(path) {
+  let cleanedPath = path.replace("../", "");
+  cleanedPath = path === "/" ? "/index.html" : path;
+
+  let content, httpCode, fileType;
+  try {
+    content = fs.readFileSync("app" + cleanedPath);
+    let mime_type = mime.lookup(cleanedPath);
+    fileType = mime_type ? mime_type : "application/octet-stream";
+    httpCode = 200;
+  } catch (e) {
+    if (e.code === "ENOENT") {
+      content = "That file doesn't exist";
+      fileType = "text/plain";
+      httpCode = 404;
+    } else {
+      console.log(e);
+      content = "Uh oh, something went wrong on the server";
+      fileType = "text/plain";
+      httpCode = 500;
+    }
+  }
+
+  return {data: content, code: httpCode, type: fileType};
+}
