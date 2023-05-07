6457304a9bbaa70ebfa6722a77042b189d85a0c5
= <sh843173@dal.ca>
2023-05-07 20:02:46 -0300

    Import bootstrap

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,6 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import './globals.css';
+import FooterScripts from "./footer-scripts";
 
 export const metadata = {
   title: 'Taskursion',
@@ -8,7 +10,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <FooterScripts />
+      </body>
     </html>
   )
 }
