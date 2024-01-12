2e0bb011eb8d0a64c22d866289bb9452cd42adfe
= <sh843173@dal.ca>
2024-01-12 16:50:14 -0400

    Make BorderHeader clickable

--- src/components/BorderHeaders/BorderHeader.jsx
+++ src/components/BorderHeaders/BorderHeader.jsx
@@ -1,22 +1,44 @@
 "use client";
 
-import { Box, useTheme } from "@mui/material";
+import { Box, ButtonBase, Link, useTheme } from "@mui/material";
+import NextLink from "next/link";
 
-export default function BorderHeader({ headerComponent, children }) {
+export default function BorderHeader({ primaryHeaderComponent, children }) {
     const theme = useTheme();
     const isDarkMode = theme.palette.mode === 'dark';
 
+    let [headerComponent, headerLink] = [<></>, <></>];
+    if (primaryHeaderComponent) {
+        headerComponent = <ButtonBase
+            sx={{ display: 'unset', textAlign: 'unset', p: 1, ':focus': { outline: 'solid' } }}
+            href={primaryHeaderComponent.linkTarget}
+        >
+            {primaryHeaderComponent.component}
+        </ButtonBase>;
+        headerLink = <Link
+            component={NextLink}
+            href={primaryHeaderComponent.linkTarget}
+            height='100%'
+            position='absolute'
+            top={0}
+            bottom={0}
+            left={0}
+            right={0}
+            zIndex={1}
+            tabIndex={-1}
+        />;
+    }
+
     return (<Box    // the "outer box" that represents the border
-        display={'flex'}
-        flexDirection={'column'}
-        height={'100vh'}
+        display='flex'
+        flexDirection='column'
+        height='100vh'
         bgcolor={theme.palette.secondary.main}
         color={theme.palette.secondary.contrastText}
         p={1}
     >
-        <Box p={1}>    
-            {headerComponent}
-        </Box>
+        {headerComponent}
+        {headerLink}
 
         <Box        // the "inner box" within the border that contains the content
             height={'100%'}
@@ -25,6 +47,7 @@ export default function BorderHeader({ headerComponent, children }) {
             color={theme.palette.getContrastText(theme.palette.background.default)}
             borderRadius={theme.shape.borderRadius}
             boxShadow={'4px 4px 10px black inset'}
+            zIndex={2}
         >
             <Box    /*
                     an "inner inner box" with a scrollbar. This needs to be its own, inner div
