b81d37b4f61410451b9e876889b51984d6af9afe
= <sh843173@dal.ca>
2024-01-12 22:16:20 -0400

    Add frontend validation to signup page

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,11 +1,75 @@
 "use client";
 
 import { useTheme } from "@emotion/react";
-import { Button, Container, Paper, TextField, Typography, useMediaQuery } from "@mui/material";
+import { Button, Container, Paper, TextField, Tooltip, Typography, useMediaQuery } from "@mui/material";
+import { useState } from "react";
+
+const PASS_REGEX = /(?=.*[^A-Za-z0-9_ \t\r\n\v\f])(?=.*\d)(?=.*[A-Z])(?=.*[a-z])/;
 
 export default function Signup() {
     const theme = useTheme();
     const isSmallScreen = useMediaQuery(theme => theme.breakpoints.down('sm'));
+
+    const [formData, setFormData] = useState({name: "", email: "", pass: ""});
+    const [formError, setFormError] = useState({name: "", email: "", pass: ""});
+    const [wasPassLengthGreaterThan7, setWasPassLengthGreaterThan7] = useState(false);
+    
+    const handleInputChange = e => {
+        setFormData({...formData, [e.target.name]: e.target.value});
+    }
+
+    const handleNameChange = e => {
+        handleInputChange(e);
+        const name = e.target.value;
+
+        if (name.length > 255) {
+            setFormError({...formError,
+                name: "This name is too long! Please use at most 255 characters."
+            });
+        } else {
+            setFormError({...formError,
+                name: ""
+            });
+        }
+    }
+
+    const handleEmailChange = e => {
+        handleInputChange(e);
+        const email = e.target.value;
+
+        if (email.length > 254) {
+            setFormError({...formError,
+                email: "This email is too long! Email addresses must not exceed 254 characters."
+            });
+        } else {
+            setFormError({...formError,
+                email: ""
+            });
+        }
+    }
+
+    const handlePassChange = e => {
+        handleInputChange(e);
+        const pass = e.target.value;
+
+        if (pass.length > 7 && !wasPassLengthGreaterThan7) {
+            setWasPassLengthGreaterThan7(true);
+        }
+
+        if (pass.length < 8 && wasPassLengthGreaterThan7) {
+            setFormError({...formError,
+                pass: "This password is too short! It needs to have at least 8 characters."
+            });
+        } else if (!PASS_REGEX.test(pass) && wasPassLengthGreaterThan7) {
+            setFormError({...formError,
+                pass: "This password doesn't meet the requirements"
+            });
+        } else {
+            setFormError({...formError,
+                pass: ""
+            });
+        }
+    }
 
     return (<Container
         maxWidth='sm'
@@ -17,25 +81,47 @@ export default function Signup() {
                 fullWidth
                 variant='filled'
                 label="Name"
+                name="name"
                 margin={isSmallScreen ? 'dense' : 'normal'}
                 size={isSmallScreen ? 'small' : 'medium'}
+                value={formData.name}
+                onChange={handleNameChange}
+                error={formError.name !== ""}
+                helperText={formError.name}
             />
             <TextField
                 fullWidth
                 variant='filled'
                 label="Email"
+                name="email"
                 type='email'
                 margin={isSmallScreen ? 'dense' : 'normal'}
                 size={isSmallScreen ? 'small' : 'medium'}
+                value={formData.email}
+                onChange={handleEmailChange}
+                error={formError.email !== ""}
+                helperText={formError.email}
             />
-            <TextField
-                fullWidth
-                variant='filled'
-                label="Password"
-                type='password'
-                margin={isSmallScreen ? 'dense' : 'normal'}
-                size={isSmallScreen ? 'small' : 'medium'}
-            />
+            <Tooltip 
+                title="Must have at least 8 characters, a symbol, a number, an uppercase character,
+                and a lowercase character"
+                disableHoverListener
+                placement='top-start'
+            >
+                <TextField
+                    fullWidth
+                    variant='filled'
+                    label="Password"
+                    name="pass"
+                    type='password'
+                    margin={isSmallScreen ? 'dense' : 'normal'}
+                    size={isSmallScreen ? 'small' : 'medium'}
+                    value={formData.password}
+                    onChange={handlePassChange}
+                    error={formError.pass !== ""}
+                    helperText={formError.pass}
+                />
+            </Tooltip>
             <Button
                 type='submit'
                 variant='contained'
