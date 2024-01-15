ca5310d9b85714c4b3bc5eb64b3e7f786441e35d
= <sh843173@dal.ca>
2024-01-14 20:33:36 -0400

    Move /user to /home path

--- src/app/(curr-user)/home/page.jsx
+++ src/app/(curr-user)/home/page.jsx
@@ -1,9 +1,7 @@
 import Dashboard from "@/src/components/Dashboard";
 
 export default function AccountHome() {
-    return (
-        <main>
-            <Dashboard />
-        </main>
-    );
+    return (<main>
+        <Dashboard />
+    </main>);
 }
