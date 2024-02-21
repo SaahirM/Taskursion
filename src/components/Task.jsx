43f03124700fdf67c41eeb9dd6bf841f32667797
= <sh843173@dal.ca>
2024-02-20 21:34:32 -0400

    Add back arrow icon

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,12 +1,16 @@
 import Link from "next/link";
 import BorderHeader from "./BorderHeaders/BorderHeader";
 import BorderLogo from "./BorderHeaders/BorderLogo";
+import ChevronLeftRoundedIcon from '@mui/icons-material/ChevronLeftRounded';
 import { Typography } from "@mui/material";
 
 export default function Task({ taskId }) {
-    const backLinkComponent = <Typography variant='h4' component='p' noWrap width='100%'>
-        &lt; Back to <b>Task 2</b>
-    </Typography>;
+    const backLinkComponent = <>
+        <ChevronLeftRoundedIcon fontSize='large' />
+        <Typography variant='h4' component='p' noWrap width='100%'>
+            Back to <b>Task 2</b>
+        </Typography>
+    </>;
 
     return (<BorderHeader
         primaryHeaderComponent={{ component: backLinkComponent, linkTarget: "/home/task/2" }}
