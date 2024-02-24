979490148a463ba31f70c3f216cae8b321853929
= <sh843173@dal.ca>
2024-02-24 18:39:39 -0400

    Fix MongoClientClosed bug in development

--- src/util/db.js
+++ src/util/db.js
@@ -1,4 +1,20 @@
 const { MongoClient } = require('mongodb');
 
-const client = new MongoClient(process.env.MONGO_CONN_STR);
+let client;
+
+// In development mode, use a global variable so that the value is preserved across
+// module reloads caused by HMR (Hot Module Replacement). This code is from:
+// https://github.com/vercel/next.js/blob/canary/examples/with-mongodb/lib/mongodb.ts
+if (process.env.NODE_ENV === "development") {
+    let globalWithMongo = global;
+
+    if (!globalWithMongo._mongoClient) {
+        client = new MongoClient(process.env.MONGO_CONN_STR);
+        globalWithMongo._mongoClient = client;
+    }
+    client = globalWithMongo._mongoClient;
+} else {
+    client = new MongoClient(process.env.MONGO_CONN_STR);
+}
+
 export default client;
