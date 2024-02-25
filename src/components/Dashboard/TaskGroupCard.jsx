452b95475a03a83d19c8b718435d36c520edd124
= <sh843173@dal.ca>
2024-02-24 21:06:59 -0400

    Cut homepage root-task text if it's too long

--- src/components/Dashboard/TaskGroupCard.jsx
+++ src/components/Dashboard/TaskGroupCard.jsx
@@ -3,10 +3,24 @@ import Link from "next/link";
 
 export default function TaskGroupCard({ taskGroup }) {
     return (<Card elevation={4}>
-        <CardActionArea href={`/home/task/${taskGroup._id.task_id}`} LinkComponent={Link}>    
+        <CardActionArea href={`/home/task/${taskGroup._id.task_id}`} LinkComponent={Link}>
             <CardContent>
-                <Typography variant='h4' component='h3'>{taskGroup.task_title}</Typography>
-                <Typography variant='body1'>{taskGroup.task_desc}</Typography>
+                <Typography variant='h4' component='h3' noWrap>{taskGroup.task_title}</Typography>
+                <Typography
+                    variant='body1'
+                    
+                    // Cut text off if it exceeds four lines. Code adapted from:
+                    // https://stackoverflow.com/a/72279983
+                    sx={{
+                        overflow: "hidden",
+                        textOverflow: "ellipsis",
+                        display: "-webkit-box",
+                        WebkitLineClamp: "4",
+                        WebkitBoxOrient: "vertical",
+                    }}
+                >
+                    {taskGroup.task_desc}
+                </Typography>
             </CardContent>
         </CardActionArea>
     </Card>);
