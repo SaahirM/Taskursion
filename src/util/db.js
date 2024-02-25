56d2569a9f83d525fdfc60c03da744e5e376bf38
= <sh843173@dal.ca>
2024-02-24 20:08:32 -0400

    Rename global mongo-client variable

--- src/util/db.js
+++ src/util/db.js
@@ -6,13 +6,11 @@ let client;
 // module reloads caused by HMR (Hot Module Replacement). This code is from:
 // https://github.com/vercel/next.js/blob/canary/examples/with-mongodb/lib/mongodb.ts
 if (process.env.NODE_ENV === "development") {
-    let globalWithMongo = global;
-
-    if (!globalWithMongo._mongoClient) {
+    if (!global._mongoClient) {
         client = new MongoClient(process.env.MONGO_CONN_STR);
-        globalWithMongo._mongoClient = client;
+        global._mongoClient = client;
     }
-    client = globalWithMongo._mongoClient;
+    client = global._mongoClient;
 } else {
     client = new MongoClient(process.env.MONGO_CONN_STR);
 }
