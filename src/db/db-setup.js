5bf064161714c2c6b9f0f737b87d8677fc48d2af
= <sh843173@dal.ca>
2024-02-26 22:37:43 -0400

    Update db-setup to not always manually load env vars to fix vercel build error

--- src/db/db-setup.js
+++ src/db/db-setup.js
@@ -1,20 +1,24 @@
 const { MongoClient } = require('mongodb');
-const importEnvResult = require('dotenv').config();
-
-if (importEnvResult.error) {
-    throw new Error(
-        "Failed to load environment variables\n" +
-        importEnvResult.error.message
-    );
-}
+const dotenv = require('dotenv');
 
 if (!process.env.MONGO_CONN_STR) {
-    throw new Error(
-        "Cannot find database connection string in environment variables. " +
-        "Please refer to README.md and make sure you've carefully followed " +
-        "its instructions. You should have a .env file with a " +
-        "MONGO_CONN_STR variable"
-    );
+    const importEnvResult = dotenv.config();
+    if (importEnvResult.error) {
+        throw new Error(
+            "Failed to load environment variables\n" +
+            importEnvResult.error.message
+        );
+    }
+
+    if (!process.env.MONGO_CONN_STR) {    
+        throw new Error(
+            "Cannot find database connection string in environment variables. " +
+            "Please refer to README.md and make sure you've carefully followed " +
+            "its instructions. You should have a .env file with a " +
+            "MONGO_CONN_STR variable"
+        );
+    }
+
 }
 
 const unconnectedClient = new MongoClient(process.env.MONGO_CONN_STR);
