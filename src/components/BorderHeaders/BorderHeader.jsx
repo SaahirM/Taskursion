a054ccc00a5df1cace9c2ba8217b1ea74488aed3
= <sh843173@dal.ca>
2024-05-03 20:32:34 -0300

    fix scrollbar on non-firefox browsers

--- src/components/BorderHeaders/BorderHeader.jsx
+++ src/components/BorderHeaders/BorderHeader.jsx
@@ -61,28 +61,34 @@ export default function BorderHeader({
                         position: 'relative',
                         overflowY: 'auto',
 
-                        scrollbarColor: theme.vars.palette.primary.light + " white",
-                        [theme.getColorSchemeSelector('dark')]: {
-                            scrollbarColor: theme.vars.palette.primary.dark + " black"
+                        // firefox
+                        '@supports not selector(::-webkit-scrollbar)': {
+                            scrollbarColor: theme.vars.palette.primary.light + " white",
+                            [theme.getColorSchemeSelector('dark')]: {
+                                scrollbarColor: theme.vars.palette.primary.dark + " black"
+                            },
                         },
 
-                        '&::-webkit-scrollbar': {
-                            width: 10,
-                            borderRadius: 5,
+                        // chrome, safari, etc
+                        '@supports selector(::-webkit-scrollbar)': {
+                            '&::-webkit-scrollbar': {
+                                width: 10,
+                                borderRadius: 5,
 
-                            backgroundColor: 'white',
-                            [theme.getColorSchemeSelector('dark')]: {
-                                backgroundColor: 'black'
-                            }
-                        },
-                        '&::-webkit-scrollbar-thumb': {
-                            backgroundColor: theme.vars.palette.primary.main,
-                            borderRadius: 5
-                        },
-                        '&::-webkit-scrollbar-button': {
-                            backgroundColor: theme.vars.palette.primary.light,
-                            [theme.getColorSchemeSelector('dark')]: {
-                                backgroundColor: theme.vars.palette.primary.dark
+                                backgroundColor: 'white',
+                                [theme.getColorSchemeSelector('dark')]: {
+                                    backgroundColor: 'black'
+                                }
+                            },
+                            '&::-webkit-scrollbar-thumb': {
+                                backgroundColor: theme.vars.palette.primary.main,
+                                borderRadius: 5
+                            },
+                            '&::-webkit-scrollbar-button': {
+                                backgroundColor: theme.vars.palette.primary.light,
+                                [theme.getColorSchemeSelector('dark')]: {
+                                    backgroundColor: theme.vars.palette.primary.dark
+                                }
                             }
                         }
                     }}
