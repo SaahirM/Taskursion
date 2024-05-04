5962a51320c09e248bc6196b885bdf7513601483
= <sh843173@dal.ca>
2024-05-04 19:58:45 -0300

    re-implement subtask-creation functionality

--- src/components/Task/ChildTaskList.jsx
+++ src/components/Task/ChildTaskList.jsx
@@ -18,42 +18,10 @@ export default function ChildTaskList({ childTasksPromise, parentId, saveTask })
         });
     }, [childTasksPromise]);
 
-    const addTask = taskTitle => {
-        if (taskTitle === "") return;
-
-        fetch("/api/task", {
-            method: 'POST',
-            body: JSON.stringify({
-                task_parent_id: parentId,
-                task_title: taskTitle,
-                task_desc: ""
-            })
-        })
-            .then(async res => {
-                if (!res.ok) {
-                    const error = await res.text();
-                    if (error !== "") {
-                        throw new Error(error);
-                    }
-                    throw new Error(`Server responded with a ${res.status} status code`);
-                }
-                return await res.json();
-            })
-            .then(data => {
-                setChildTasks([...childTasks, data]);
-                setCreatedTask("");
-            })
-            .catch(e => {
-                const message = e.message ??
-                    "An unexpected error occurred while communicating with the server";
-                toast(message);
-            });
-    };
-
     if (loading) return <Skeleton animation='wave' height={200} />;
 
     return (<>
-        <CreateChildTaskBtn addTask={addTask} />
+        <CreateChildTaskBtn parentId={parentId} setChildTasks={setChildTasks} />
         <Stack divider={<Divider />}>
             {childTasks.length === 0
                 ? <Divider component='div' role='presentation'><Typography variant='body1' textAlign='center'>
--- src/components/Task/CreateChildTaskBtn.jsx
+++ src/components/Task/CreateChildTaskBtn.jsx
@@ -1,14 +1,57 @@
 import { AddBoxRounded } from "@mui/icons-material";
 import { Card, CardContent, CardHeader, Checkbox, FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
+import { useState } from "react";
 
-export default function CreateChildTaskBtn({ addTask }) {
+export default function CreateChildTaskBtn({ parentId, setChildTasks }) {
+    const [title, setTitle] = useState("");
+    const [checked, setChecked] = useState(false);
+
+    const addTask = () => {
+        if (title === "") return;
+
+        fetch("/api/task", {
+            method: 'POST',
+            body: JSON.stringify({
+                task_parent_id: parentId,
+                task_title: title,
+                task_desc: "",
+                task_completed: checked
+            })
+        })
+            .then(async res => {
+                if (!res.ok) {
+                    const error = await res.text();
+                    if (error !== "") {
+                        throw new Error(error);
+                    }
+                    throw new Error(`Server responded with a ${res.status} status code`);
+                }
+                return await res.json();
+            })
+            .then(data => {
+                setChildTasks(oldChildTasks => [...oldChildTasks, data]);
+                setTitle("");
+                setChecked(false);
+            })
+            .catch(e => {
+                const message = e.message ??
+                    "An unexpected error occurred while communicating with the server";
+                toast(message);
+            });
+    };
+
     return (<Card elevation={3} sx={{ mt: 3, mb: 1 }}>
         <CardHeader sx={{ pb: 0 }} title="Create subtask" />
-        <CardContent sx={{ py: 1 }}>
+        <CardContent sx={{ py: 1, ':last-child': { pb: 2 } }}>
             <Grid container gap={1}>
-                <Grid xs='auto'>
-                    <Checkbox edge='start' size='large' />
+                <Grid xs='auto' display='flex' alignItems='center'>
+                    <Checkbox
+                        edge='start'
+                        size='large'
+                        checked={checked}
+                        onChange={e => setChecked(e.target.checked)}
+                    />
                 </Grid>
                 <Grid xs>
                     <FormControl fullWidth>
@@ -17,8 +60,10 @@ export default function CreateChildTaskBtn({ addTask }) {
                             id="create-subtask-input"
                             label="Title"
                             placeholder="Plan everything out..."
+                            value={title}
+                            onChange={e => setTitle(e.target.value)}
                             endAdornment={<InputAdornment position='end'>
-                                <IconButton edge='end' aria-label="Add subtask">
+                                <IconButton edge='end' aria-label="Add subtask" onClick={addTask}>
                                     <AddBoxRounded />
                                 </IconButton>
                             </InputAdornment>}
